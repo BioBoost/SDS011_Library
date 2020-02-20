86eec926c18e41b20dd3c4cd9fa969086075e8a7
Martijn Everaert <martijn.everaert@gmail.com>
2020-02-20 23:10:25 +0100

    added function 'reshuffledata' and changed layout like in class

--- src/SDS011_Library.cpp
+++ src/SDS011_Library.cpp
@@ -1,70 +1,90 @@
 #include SDS011_Library.h
 #include "mbed.h"
 
+namespace SDS011{
+    SDS011(PinName pinTXdevice, PinName pinRXdevice){
+        _pintTXdevice = pintTXdevice;
+        _pintRXdevice = pintRXdevice;
+        Serial sensor(_pinTXdevice,_pinTXdevice);
+        sensor.baud(9600);
+    }
 
-void SDS011::SDS011(PinName pinTXdevice, PinName pinRXdevice){
-    _pintTXdevice = pintTXdevice;
-    _pintRXdevice = pintRXdevice;
-    Serial sensor(_pinTXdevice,_pinTXdevice);
-    sensor.baud(9600);
-}
-
-void SDS011::read(){
-
-    if (sensor.readable()){
-        sensor.gets((char*) buffer, PACKET_SIZE);
-
-        if(buffer[0] != 170)
-            while(buffer[0] !=170){
-                reshuffleData();
+    void read(void){
+        if (sensor.readable()){
+            sensor.gets((char*) buffer, PACKET_SIZE);   
+            if(buffer[0] != 170){
+                while(buffer[0] !=170){
+                    reshuffleData();
+                }
             }
-        if(buffer[9] != 171)
-            dataError = true;
-            
-
-
+            if(buffer[9] != 171){
+                dataError = true;
+            }
+        }
+        //PM25Value = buffer[3] * 256 + buffer[2]/10.0;
+        //PM10Value = buffer[5] *256 + buffer[4]/10.0;
+        //idByte = buffer[6] + buffer[7]*256;
     }
 
-    //PM25Value = buffer[3] * 256 + buffer[2]/10.0;
-    //PM10Value = buffer[5] *256 + buffer[4]/10.0;
-    //idByte = buffer[6] + buffer[7]*256;
-
-}
-
-void SDS011::sendDataToPc(){
-    Serial pc(USBTX,USBRX);
-    pc.printf("the sensorID is %X %X \r\n", idByte);
-    pc.printf("The air contains %.1lf µg/m³ of PM2.5 \r\n", PM25Value);
-    pc.printf("The air contains %.1lf µg/m³ of PM10 \r\n", PM10Value);
-    if(correctChecksum == true){
-        pc.printf("The checksum is true, data is correct");
-    } else {
-        pc.printf("The checksum is false, data is corrupt")
+    void sendDataToPc(){
+        Serial pc(USBTX,USBRX);
+        pc.printf("the sensorID is %X %X \r\n", idByte);
+        pc.printf("The air contains %.1lf µg/m³ of PM2.5 \r\n", PM25Value);
+        pc.printf("The air contains %.1lf µg/m³ of PM10 \r\n", PM10Value);
+        if(correctChecksum == true){
+            pc.printf("The checksum is true, data is correct");
+        } else {
+            pc.printf("The checksum is false, data is corrupt")
+        }
     }
-  
 
-}
-
-void SDS011::sleep(){
+    void sleep(void){
     /* TODO */
+    }
+
+    void reshuffleData(){
+        while(buffer[0] != "C0"){
+            /* schuif de hele array met een naar links */
+            int hulp = buffer[0];       //anders verlies van data
+            for(int t=0; t<PACKET_SIZE-1; t++){
+                buffer[t] = buffer[t-1];
+            }
+            buffer[PACKET_SIZE] = hulp;
+        }
+    }
+
+    double getPM25Value(){
+        return PM25Value;
+    }
+
+    double getPM10Value(){
+        return PM10Value;
+    }
+
+    int getIdByte(){
+        return idByte;
+    }
+
+    bool correctChecksum(){       
+        if(0){
+            return true;
+        }
+
+        return false;
+    }
+
 }
 
-void SDS011::reshuffleData(){
 
-}
 
-double SDS011::getPM25Value(){
-    return PM25Value;
-}
 
-double SDS011::getPM10Value(){
-    return PM10Value;
-}
 
-int SDS011::getIdByte(){
-    return idByte;
-}
 
-bool SDS011::correctChecksum(){
 
-}
+
+void SDS011::
+
+void SDS011::
+
+double SDS011::
+
--- src/SDS011_Library.h
+++ src/SDS011_Library.h
@@ -1,28 +1,30 @@
 #pragma once
 #include "mbed.h"
 
-class SDS011
-{
-    public:
-        SDS011(PinName pinTXDevice, PinName pinRXDevice);
-        double getPM25Value();
-        double getPM10Value();
-        int getIdByte();
-        bool correctChecksum();
-        void sendDataToPc();
-        void read();
-        void sleep();
+using namespace SDS011{
+    class SDS011
+    {
+        public:
+            SDS011(PinName pinTXDevice, PinName pinRXDevice);
+            double getPM25Value();
+            double getPM10Value();
+            int getIdByte();
+            bool correctChecksum();
+            void sendDataToPc();
+            void read();
+            void sleep();
     
-    private:
-        const unsigned int PACKET_SIZE = 11;
+        private:
+            const unsigned int PACKET_SIZE = 11;
 
-        uint8_t buffer[PACKET_SIZE];
-        double PM25Value;
-        double PM10Value;
-        int idByte;
-        int receivedCheckSum;
-        bool dataError = false;
-        DigitalIn _pinRXDevice;
-        DigitalOut _pinTXDevice;
-        void reshuffleData();
+            uint8_t buffer[PACKET_SIZE];
+            double PM25Value;
+            double PM10Value;
+            int idByte;
+            int receivedCheckSum;
+            bool dataError = false;
+            DigitalIn _pinRXDevice;
+            DigitalOut _pinTXDevice;
+            void reshuffleData();
+    }
 }
